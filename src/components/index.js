c3c030828b8e613465264c53664c187db8cb2ce1
Casey Calkins <a.caseycalkins@outlook.com>
2020-11-18 19:51:08 -0600

    Created cards, fetched covid data

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+// Exporting components to use in App.js
+export {default as Cards} from './Cards/Cards';
+export {default as Chart} from './Chart/Chart';
+export {default as CountryPicker} from './CountryPicker/CountryPicker';
